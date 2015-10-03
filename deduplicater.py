2bcf0e3dfe6077710424d498662a45e69c425667
Taylor Lloyd <taylor@taylorlloyd.ca>
2015-10-03 10:39:34 -0400

    Generate buckets for each predicate

--- deduplicater.py
+++ deduplicater.py
@@ -1,4 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+from math import sqrt
 
 sparql = SPARQLWrapper('http://husky-big.cs.uwaterloo.ca:8890/sparql')
 dataset = '<http://data.nytimes.com>'
@@ -40,5 +41,28 @@ preds = map((lambda r: r['p']['value']), data['results']['bindings'])
 print('%d predicates found.' % len(preds))
 
 for p in preds:
-    print('Predicate: ' + p)
+    pdata = all_data_for_pred(dataset, p)
+    print('Predicate %s: %d entries' % (p, len(pdata['results']['bindings'])))
+    max_bucket_size = sqrt(len(pdata['results']['bindings']))
+    buckets = {}
+
+    # Sort entries into hashed buckets
+    for d in pdata['results']['bindings']:
+        subject = d['s']['value']
+        obj = d['o']['value']
+        h = hash(obj)
+        if(h not in buckets):
+            buckets[h] = []
+        buckets[h].append(subject)
+
+    # Print out bucket metrics
+    print("%d buckets generated" % len(buckets.keys()))
+    usable = 0
+    for i, val in enumerate(buckets):
+        size = len(buckets[val])
+        if(size > 1 and size < max_bucket_size):
+          print("  bucket %d: %d entries" % (i, size))
+
+
+
 
