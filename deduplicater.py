869452c08453319ca218806f1f857a8c14d3a4ab
Taylor Lloyd <taylor@taylorlloyd.ca>
2015-10-03 10:05:13 -0400

    Extract predicates from the response

--- deduplicater.py
+++ deduplicater.py
@@ -1,28 +1,35 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 sparql = SPARQLWrapper('http://husky-big.cs.uwaterloo.ca:8890/sparql')
-dataset = '<http://data.nytimes.com/>'
+dataset = '<http://data.nytimes.com>'
 
 def all_data(dataset):
   sparql.setQuery("""
     select *
     from %s
     where {
-      ?s ?p ?o 
+      ?s ?p ?o
     }""" % dataset)
   sparql.setReturnFormat(JSON)
   return sparql.query().convert()
-  
+
 def all_predicates(dataset):
   sparql.setQuery("""
     select distinct ?p
     from %s
     where {
-      ?s ?p ?o 
+      ?s ?p ?o
     }""" % dataset)
   sparql.setReturnFormat(JSON)
   return sparql.query().convert()
 
 
-data = all_data(dataset)
+# Begin by retrieving all available predicates
+data = all_predicates(dataset)
+preds = map((lambda r: r['p']['value']), data['results']['bindings'])
+
+print('%d predicates found.' % len(preds))
+
+for p in preds:
+    print('Predicate: ' + p)
 
