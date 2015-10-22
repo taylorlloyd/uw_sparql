975772ada20b28fad75bc4dc641c01310a75dfff
Taylor Lloyd <taylor@taylorlloyd.ca>
2015-10-22 10:47:47 -0600

    Add documentation and pretty-printing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # uw_sparql
+
+This code reflects efforts to deduplicate data found in RDF data by matching subjects in
+(Subject, Predicate, Object) tuples.
+
+Because pairwise comparisons are expensive, the algorithm works as follows:
+
+create an empty hashmap M
+for each unique predicate p:
+    for each tuple (s, pred, o) where pred = p:
+        M[hash(o)] += s
+    for each list l in M:
+        if size(l) < sqrt(all entries) && size(l) > 1:
+            pairwise-compare all subjects in l
+
+For pairwise comparisons, we use a weighted edit distance formula.
+Our comparison functions were not yet well explored, and need more work.
--- deduplicater.py
+++ deduplicater.py
@@ -113,6 +113,7 @@ def compare(a,b):
     elif key in a or key in b:
       compsum += 1.0
       count += 1
+  print(compsum / count)
   return compsum / count <= 0.05
 
 
@@ -188,11 +189,11 @@ def pairwise_cmp(subjects):
 
 # Begin by retrieving all available predicates
 preds = all_predicates(dataset)
-#print('%d predicates found.' % len(preds))
+print('%d predicates found.' % len(preds))
 
 for p in preds:
     pdata = all_data_for_pred(dataset, p)
-    #print('Predicate %s: %d entries' % (p, len(pdata)))
+    print('Predicate %s: %d entries' % (p, len(pdata)))
     max_bucket_size = sqrt(len(pdata))
     buckets = {}
 
@@ -206,12 +207,12 @@ for p in preds:
         buckets[h].append(subject)
 
     # Print out bucket metrics
-    #print("%d buckets generated" % len(buckets.keys()))
+    print("%d buckets generated" % len(buckets.keys()))
     usable = 0
     for i, val in enumerate(buckets):
         size = len(buckets[val])
         if(size > 1 and size < max_bucket_size):
-          #print("  bucket %d: %d entries" % (i, size))
+          print("  bucket %d: %d entries" % (i, size))
           bucket = all_data_for_subjects(dataset, buckets[val])
           pairwise_cmp(bucket)
 
